e040af92b79e29fccc7c0c7f0b4a1bb018d7f6d7
Himanshu Patil <25838799+mehimanshupatil@users.noreply.github.com>
2025-07-12 20:02:38 +0530

    Convert to PWA with rupee SVG logo and mobile fixes

--- next.config.js
+++ next.config.js
@@ -5,9 +5,15 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
 });
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
